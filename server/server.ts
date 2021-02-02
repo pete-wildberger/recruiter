251fcd42ccd8464d4fb7952d3905a9ef6e741189
Peter Wildberger <pete.wildberger@gmail.com>
2021-02-01 21:17:31 -0600

    add server

---
+++ server/server.ts
@@ -0,0 +1,35 @@
+import * as express from 'express';
+import * as path from 'path';
+import * as compression from 'compression';
+import { Request, Response } from 'express';
+
+class App {
+  private PORT: string | number;
+  private express: express.Application = express();
+  constructor(port: string | number) {
+    this.PORT = port;
+    this.middleware();
+    this.routes();
+    this.listen();
+  }
+
+  middleware() {
+    this.express.use(express.static('./dist/recruiter'));
+    this.express.use(compression());
+  }
+
+  routes() {
+    this.express.get('*', (req: Request, res: Response) => {
+      res.sendFile(path.join(__dirname, '../recruiter/index.html'));
+    });
+  }
+
+  listen() {
+    this.express.listen(this.PORT, () => {
+      console.log(`TennisONE fully operational on ${this.PORT}`);
+    });
+  }
+}
+
+const PORT: string | number = process.env.PORT || 8080;
+const server = new App(PORT);
